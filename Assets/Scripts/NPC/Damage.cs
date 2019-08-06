0b232555a0b86a3746f6559561a3e0aba641e8cf
shubbart <shawn.hubbart@gmail.com>
2019-08-06 16:03:56 -0600

    Health and Damage implemented

---
+++ Assets/Scripts/NPC/Damage.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DamageType
+{
+    Physical,
+    Fire,
+    Ice,
+    Lightning,
+    Poison,
+}
+
+public class Damage : MonoBehaviour
+{
+    public float damage;
+    [SerializeField] DamageType myType;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if(collision.tag == "Player")
+    //    {
+    //        collision.GetComponent<SlimeStats>().DealDamage(damage, myType);
+    //    }
+    //}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<SlimeStats>().DealDamage(damage, myType);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/SlimeStats.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeStats : MonoBehaviour
+{
+
+    public float health;
+    // Defense = physical, MagicDefense is base magical protection, Elemental defenses are magic defense multipliers
+    public float defense, magicDefense, fireDefense, iceDefense, lightningDefense, poisonDefense;
+
+    void Start()
+    {
+        Debug.Log("Health: " + health);
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void DealDamage(float damage, DamageType damageType)
+    {       
+        if(damageType == DamageType.Physical)
+        {
+            if (CalculatePhysicalDamage(damage) > 1)
+                health -= CalculatePhysicalDamage(damage);
+            else
+                health -= 1;
+        }
+        if (damageType == DamageType.Fire)
+        {
+            if (CalculateFireDamage(damage) > 1)
+                health -= CalculateFireDamage(damage);
+            else
+                health -= 1;
+        }
+        if (damageType == DamageType.Ice)
+        {
+            if (CalculateIceDamage(damage) > 1)
+                health -= CalculateIceDamage(damage);
+            else
+                health -= 1;
+        }
+        if (damageType == DamageType.Lightning)
+        {
+            if (CalculateLightningDamage(damage) > 1)
+                health -= CalculateLightningDamage(damage);
+            else
+                health -= 1;
+        }
+        if (damageType == DamageType.Poison)
+        {
+            if (CalculatePoisonDamage(damage) > 1)
+                health -= CalculatePoisonDamage(damage);
+            else
+                health -= 1;
+        }
+        Debug.Log("Health: " + health);
+
+        if (health <= 0)
+            DoDeath();
+    }
+
+    void DoDeath()
+    {
+        Debug.Log("Player Died!");
+        Destroy(gameObject);
+    }
+
+    float CalculatePhysicalDamage(float damage)
+    {
+        return damage - defense;
+    }
+
+    float CalculateFireDamage(float damage)
+    {
+        return damage - fireDefense * magicDefense;
+    }
+
+    float CalculateIceDamage(float damage)
+    {
+        return damage - iceDefense * magicDefense;
+    }
+
+    float CalculateLightningDamage(float damage)
+    {
+        return damage - lightningDefense * magicDefense;
+    }
+
+    float CalculatePoisonDamage(float damage)
+    {
+        return damage - poisonDefense * magicDefense;
+    }
+}
