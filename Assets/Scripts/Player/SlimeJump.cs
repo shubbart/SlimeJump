2d8163fdb4ae0b87aaa0430d4aa5dc56de5a338d
shubbart <shawn.hubbart@gmail.com>
2019-08-06 01:01:26 -0600

    Prototype of jumping and goal complete

---
+++ Assets/Scripts/Player/SlimeJump.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerState
+{
+    Idle,
+    Airborne,
+    Sticking,
+    Damaged,
+    Dead
+}
+
+public class SlimeJump : MonoBehaviour
+{
+    public PlayerState playerState = PlayerState.Idle;
+    public float forceMultiplier = 1;
+
+    Vector2 aim;
+    Rigidbody2D rbody;
+    float force;
+    
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        if (playerState == PlayerState.Idle)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                aim = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
+                force = Vector2.Distance(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition)) * forceMultiplier;
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                rbody.AddForce(aim * force, ForceMode2D.Impulse);
+                playerState = PlayerState.Airborne;
+            }
+        }
+        else if (playerState == PlayerState.Airborne)
+        {
+            if (rbody.velocity.y == 0)
+            {
+                playerState = PlayerState.Idle;
+            }
+        }
+    }
+}
